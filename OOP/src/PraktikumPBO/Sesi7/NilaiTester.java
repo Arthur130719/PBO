cc39ac61466acc9c39df8e599c7e54695b1c79eb
Arthur130719 <satria130719@gmail.com>
2024-11-04 10:32:02 +0700

    fresh from the oven

---
+++ OOP/src/PraktikumPBO/Sesi7/NilaiTester.java
@@ -0,0 +1,12 @@
+package PraktikumPBO.Sesi7;
+
+public class NilaiTester {
+    public static void main(String[] args) {
+        Nilai n= new Nilai ();
+        n.Quis=90;
+        n.UTS=70;
+        n.UAS=150;
+        System.out.println("NA : " +n.getNA());
+    }
+    
+}
